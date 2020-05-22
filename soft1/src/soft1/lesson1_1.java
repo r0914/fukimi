775b15157936cec39cd67bae9e65cee1975ff311
suzukirisa <suzukirisa@192.168.0.11>
2020-05-22 15:38:38 +0900

    test

--- soft1/src/soft1/lesson1_1.java
+++ soft1/src/soft1/lesson1_1.java
@@ -4,5 +4,6 @@ public class lesson1_1 {
 	public static void main(String[] args) {
 	System.out.println("わたしのすきな食べ物は洋梨です。");
 	System.out.println("理由は美味しいからです。");
+	System.out.println("原産地はヨーロッパです。");
 	}
 }
